114bf17b38afec86312ccb5b53246dd694ba95b4
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-11-13 15:45:37 +0530

    Fixed accessibility tests.

--- tests/accessibility.test.js
+++ tests/accessibility.test.js
@@ -4,15 +4,16 @@
 // Once this issue is fixed uncomment the taiko script and remove jest tests and dependency.
 
 
-const { accessibility, closeBrowser, goto, openBrowser } = require('taiko');
+const { accessibility, closeBrowser, goto, openBrowser, screenshot } = require('taiko');
 const { ok, equal } = require('assert');
 const urls = require('./urls.json');
 let excludedRules = ['landmark-one-main'];
 
 describe('accessibility', () => {
 
-  beforeAll(async () => {
+  beforeAll(async (done) => {
     await openBrowser();
+    done();
   });
 
   afterAll(async () => {
@@ -23,9 +24,10 @@ describe('accessibility', () => {
     test(`${url}`, async () => {
       jest.setTimeout(20000);
       await goto(url);
+      await screenshot({path: `./logs/screenshot-for-${url.replace(/file.*\/_build\/(.*)\.html/g,'$1').replace(/\//g,'-')}.png`})
       const audit = await accessibility.runAudit();
       let violations = audit.violations.filter(v => !excludedRules.includes(v.id))
-      ok(audit.score >= 97, `Expected: >= ${97}\nReceived:    ${audit.score}`);
+      ok(audit.score >= 97, `Expected: >= ${97}\nReceived:    ${audit.score} for ${url}`);
       equal(violations.length, 0, violations)
     });
   })
